5e90d97d842278889f5600504b0e0f13ec0f5e42
ikachanp <w.ikuro19@gmail.com>
2018-03-08 18:00:44 +0900

    none

---
+++ study/JAVA/JAVA-class1/01/script.js
@@ -0,0 +1,16 @@
+document.write("Hello World<br>");
+
+document.write("My name is Tanaka.");
+
+document.write("I'm 25 years old<br>")
+
+document.write("aiueo<br>");
+document.write("kakukukeko<br>");
+document.write("sasisuseso<br>");
+
+document.write("a<br>ba<br>ba<br>");
+document.write("<h1>a</h1><br>");
+document.write("<p>oi</p><br>");
+
+alert("move to another page");
+
