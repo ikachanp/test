2910a7daabcff4f311077c205d287fdb07cbef3e
ikachanp <37167664+ikachanp@users.noreply.github.com>
2018-05-09 13:03:12 +0900

    Add files via upload

---
+++ ProductSearchDTO.java
@@ -0,0 +1,112 @@
+package com.kensaku.dto;
+
+public class ProductSearchDTO {
+
+	public int id;
+	public int product_id;
+	public String product_name;
+	public String product_name_kana;
+	public String product_description;
+	public int category_id;
+	public int price;
+	public String image_file_path;
+	public String image_file_name;
+	public String release_date;
+	public int status;
+	public String regist_date;
+	public String update_date;
+
+	public String category_name;
+	public String category_description;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getProduct_name_kana() {
+		return product_name_kana;
+	}
+	public void setProduct_name_kana(String product_name_kana) {
+		this.product_name_kana = product_name_kana;
+	}
+	public String getProduct_description() {
+		return product_description;
+	}
+	public void setProduct_description(String product_description) {
+		this.product_description = product_description;
+	}
+	public int getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getImage_file_path() {
+		return image_file_path;
+	}
+	public void setImage_file_path(String image_file_path) {
+		this.image_file_path = image_file_path;
+	}
+	public String getImage_file_name() {
+		return image_file_name;
+	}
+	public void setImage_file_name(String image_file_name) {
+		this.image_file_name = image_file_name;
+	}
+	public String getRelease_date() {
+		return release_date;
+	}
+	public void setRelease_date(String release_date) {
+		this.release_date = release_date;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getRegist_date() {
+		return regist_date;
+	}
+	public void setRegist_date(String regist_date) {
+		this.regist_date = regist_date;
+	}
+	public String getUpdate_date() {
+		return update_date;
+	}
+	public void setUpdate_date(String update_date) {
+		this.update_date = update_date;
+	}
+	public String getCategory_name() {
+		return category_name;
+	}
+	public void setCategory_name(String category_name) {
+		this.category_name = category_name;
+	}
+	public String getCategory_description() {
+		return category_description;
+	}
+	public void setCategory_description(String category_description) {
+		this.category_description = category_description;
+	}
+
+}
